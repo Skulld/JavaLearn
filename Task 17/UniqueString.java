2bc5ae8716b87b1144e4c4e064ae65cc44b10db5
Skulld <sooretsu@gmail.com>
2021-12-29 20:16:03 +0300

    Add files via upload
    first string

--- Task 17/UniqueString.java
+++ Task 17/UniqueString.java
@@ -2,31 +2,31 @@ import java.util.Scanner;
 
 public class UniqueString
 {
-   public static void main(String[] args) 
-   {
-      Scanner in = new Scanner(System.in);
-      System.out.println("Введите кол-во строк:");
-      int n = in.nextInt();
-      String[] strs = new String[n];
-      String mostUnique = "";
-      int max = 0;
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите кол-во строк:");
+        int n = in.nextInt();
+        String[] strs = new String[n];
+        String mostUnique = "";
+        int max = 0;
 
-      for (int i = 0; i < n; i++){
-         System.out.printf("%n%s%d%s", "Строка №", i+1, ": ");
-         strs[i] = in.next();
-         String temp = strs[i].toLowerCase();
+        for (int i = 0; i < n; i++){
+            System.out.printf("%n%s%d%s", "Строка №", i+1, ": ");
+            strs[i] = in.next();
+            String temp = strs[i].toLowerCase();
             String buffer="";
-               int counter=0;
-         for (int j = 0; j < temp.length(); j++){
-            
-            if (!(buffer.contains(String.valueOf(temp.charAt(j))))) {
-               buffer += String.valueOf(temp.charAt(j));
-               counter++;}
-            max = (max < counter) ? counter : max;
-            mostUnique = (max == counter) ? strs[i] : mostUnique;
-         }
-      }
-      System.out.println(max);
-      System.out.println(mostUnique);
+            int counter=0;
+            for (int j = 0; j < temp.length(); j++){
+
+                if (!(buffer.contains(String.valueOf(temp.charAt(j))))) {
+                    buffer += String.valueOf(temp.charAt(j));
+                    counter++;}
+                mostUnique = (max < counter) ? strs[i] : mostUnique;
+                max = (max < counter) ? counter : max;
+
+            }
+        }
+        System.out.printf("%n%s%s", "Ответ: ", mostUnique);
     }
 }
