9270b736e4bbd1deda754559c1ef2f13c8cc71ea
Skulld <sooretsu@gmail.com>
2021-12-29 20:04:28 +0300

    Add files via upload
    sign check

--- Task 5/ConsoleOperation.java
+++ Task 5/ConsoleOperation.java
@@ -2,36 +2,36 @@ import java.util.Scanner;
 
 public class ConsoleOperation 
 {
-     public static void main(String[] args) 
-     { 
-       
-    Scanner numbers = new Scanner(System.in);
-    System.out.println("Введите первое число:");
-      float x = numbers.nextFloat();
-      System.out.println("Введите второе число:");
-      float y = numbers.nextFloat();
-      System.out.print("Введите знак операции, которую хотите выполнить:");
-      String sign = numbers.next();
+      public static void main(String[] args)
+    {
 
-   if (y == 0) {
-      System.out.print("На ноль делить нельзя");}
+        Scanner numbers = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        float x = numbers.nextFloat();
+        System.out.println("Введите второе число:");
+        float y = numbers.nextFloat();
+        System.out.print("Введите знак операции, которую хотите выполнить:");
+        String sign = numbers.next();
+
+        if ((sign.equals("/")) & (y == 0))  {
+        System.out.print("На ноль делить нельзя");}
    else {
-    switch (sign) {
-     case "+":
-        System.out.println(x+y);
-         break;
-     case "-":
-        System.out.println(x-y);
-         break;
-     case "*":
-        System.out.println(x*y);
-         break;
-     case "/": 
-     System.out.println(x/y);
-         break;
-      default: 
-         break;
-      }
-     }
-   }
- }
+        switch (sign) {
+            case "+":
+                System.out.println(x+y);
+                break;
+            case "-":
+                System.out.println(x-y);
+                break;
+            case "*":
+                System.out.println(x*y);
+                break;
+            case "/":
+                System.out.println(x/y);
+                break;
+            default:
+                break;
+        }
+    }
+    }
+}
