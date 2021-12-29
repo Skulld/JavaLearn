ea31cd47b191696d036068de2f7146c8ce3fa691
Skulld <sooretsu@gmail.com>
2021-12-29 19:55:21 +0300

    Add files via upload
    amount / rate

--- Task 15/Currency.java
+++ Task 15/Currency.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Currency
 {
-     public static float dollar(float rate, int amount) {
-      return (float) rate*amount;
-      }
+    public static float dollar(float rate, float amount) {
+        return (float) amount/rate;
+    }
 
-   public static void main(String[] args) 
-   {
-      Scanner in = new Scanner(System.in);
-      System.out.print("Курс доллара: ");
-      float rate = in.nextFloat();
-      System.out.print("Количество рублей: ");
-      int amount = in.nextInt();
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Курс доллара: ");
+        float rate = in.nextFloat();
+        System.out.print("Количество рублей: ");
+        int amount = in.nextInt();
 
-      float dollarvalue = dollar(rate, amount);
-      System.out.printf("%s %.2f %s", "Итого:", dollarvalue, "долларов");
-   }
- }
+        float dollarvalue = dollar(rate, amount);
+        System.out.printf("%s %.2f %s", "Итого:", dollarvalue, "долларов");
+    }
+}
